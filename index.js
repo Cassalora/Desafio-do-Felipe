b8ccc63f73aa7bcaa39ea033030fafa15be6429d
Matheus <mh7381428@gmail.com>
2024-12-08 13:39:32 -0300

    projeto

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-let nome = "Matheus"
-console.log(nome)
+let heroi = "Link" ;
 
+switch (XP) { case "1.000 XP = Ferro" :
+    console.log("Link está na liga de Ferro!") ;
+    break;
+    case "1.612 XP = Bronze" :
+        console.log("Link está na liga de Bronze!") ;
+        break;
+        case "3.489 XP = Prata" :
+            console.log("Link está na liga de Prata!")
+            break;
+            case "6.200 XP = Ouro" :
+                console.log("Link está na liga de Ouro")
+                break;
+                case "7.714 XP = Platina" :
+                    console.log("Link está na liga de Platina")
+                    break;
+                    case "8.900 XP = Ascendente" :
+                        console.log("Link está na liga Ascendente")
+                        break
+                        case "9.320 XP = Imortal" :
+                            console.log("Link está na liga Imortal")
+                            break
+                            case "Max XP = Radiante"
+                            console.log("XP Máximo alcançado! Link está na liga Radiante") ; 
+}
+                        
