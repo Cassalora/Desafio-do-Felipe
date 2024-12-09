718dcdbf575d691ed2710d9082b7d2b8b355b42e
Matheus <mh7381428@gmail.com>
2024-12-09 13:41:15 -0300

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,87 @@
-let heroi = "Platina"
-
-switch (heroi){
+//Switc Case
+​
+let link = "Link esta na "
+​
+let um = "Liga de Ferro "
+​
+let dois = "Liga de Bronze "
+​
+let tres = "Liga de Prata "
+​
+let quatro = "Liga de Ouro "
+​
+let cinco = "Liga de Platina "
+​
+let seis = "Liga Ascendente "
+​
+let sete = "Liga dos Imortais "
+​
+let oito = "Liga Radiante "
+​
+switch (link){
     
-    case "Bronze":
-        console.log("Link está na liga de Bronze!")
+    case "Ferro ":
+    console.log(link + um)
+    break
+    
+    case "Bronze ":
+        console.log(link + dois)
         break
         
-    case "Prata":
-         console.log("Link está na liga de Prata!")
+    case "Prata ":
+         console.log(link + tres)
             break
             
-    case "Ouro":
-         console.log("Link está na liga de Ouro")
+    case "Ouro ":
+         console.log(link + quatro)
             break
                 
-    case "Platina":
-         console.log("Link está na liga de Platina")
+    case "Platina ":
+         console.log(link + cinco)
             break
                     
-    case "Ascendente":
-         console.log("Link está na liga Ascendente")
+    case "Ascendente ":
+         console.log(link + seis)
             break
                         
-    case "Imortal":
-         console.log("Link está na liga Imortal")
+    case "Imortal ":
+         console.log(link + sete)
             break
                             
-    case "Radiante":
-         console.log("XP Máximo alcançado! Link está na liga Radiante") }
+    case "Radiante ":
+         console.log(link + oito)
+         break
+         }
+         
+         //Operadores
+         
+         
+         let ferro = "1 - 1000"
+         console.log(link + um + ferro)
+         
+         let bronze = "1001 - 2000"
+         console.log(link + dois + bronze)
+         
+         let prata = "2001 - 5000"
+         console.log(link + tres + prata)
+         
+         let ouro = "5001 - 7000"
+         console.log(link + quatro + ouro)
+         
+         let platina = "7001 - 8000"
+         console.log(link + cinco + platina)
+         
+         let ascendente = "8001 - 9000"
+         console.log(link + seis + ascendente)
+         
+         let imortal = "9001 - 10000"
+         console.log(link + sete + imortal)
+         
+         let radiante = "10000 - MAX"
+         console.log(link + oito + radiante)
+         
+         
+         //Laços de Repetição
+
+
+
