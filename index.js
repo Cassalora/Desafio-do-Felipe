62f258190b1007952068f53c8d23ce9c9c33833d
Matheus <mh7381428@gmail.com>
2024-12-10 10:20:45 -0300

    first commit

--- index.js
+++ index.js
@@ -1,87 +1,30 @@
-//Switc Case
-​
-let link = "Link esta na "
-​
-let um = "Liga de Ferro "
-​
-let dois = "Liga de Bronze "
-​
-let tres = "Liga de Prata "
-​
-let quatro = "Liga de Ouro "
-​
-let cinco = "Liga de Platina "
-​
-let seis = "Liga Ascendente "
-​
-let sete = "Liga dos Imortais "
-​
-let oito = "Liga Radiante "
-​
-switch (link){
-    
-    case "Ferro ":
-    console.log(link + um)
-    break
-    
-    case "Bronze ":
-        console.log(link + dois)
-        break
-        
-    case "Prata ":
-         console.log(link + tres)
-            break
-            
-    case "Ouro ":
-         console.log(link + quatro)
-            break
-                
-    case "Platina ":
-         console.log(link + cinco)
-            break
-                    
-    case "Ascendente ":
-         console.log(link + seis)
-            break
-                        
-    case "Imortal ":
-         console.log(link + sete)
-            break
-                            
-    case "Radiante ":
-         console.log(link + oito)
-         break
-         }
-         
-         //Operadores
-         
-         
-         let ferro = "1 - 1000"
-         console.log(link + um + ferro)
-         
-         let bronze = "1001 - 2000"
-         console.log(link + dois + bronze)
-         
-         let prata = "2001 - 5000"
-         console.log(link + tres + prata)
-         
-         let ouro = "5001 - 7000"
-         console.log(link + quatro + ouro)
-         
-         let platina = "7001 - 8000"
-         console.log(link + cinco + platina)
-         
-         let ascendente = "8001 - 9000"
-         console.log(link + seis + ascendente)
-         
-         let imortal = "9001 - 10000"
-         console.log(link + sete + imortal)
-         
-         let radiante = "10000 - MAX"
-         console.log(link + oito + radiante)
-         
-         
-         //Laços de Repetição
+let link = "Link está na ";
 
+let xp = 9000;
+
+let nivel;
+
+
+if (xp < 1000) {
+    nivel = "Liga de Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Liga de Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Liga de Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+    nivel = "Liga de Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Liga de Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Liga Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Liga Imortal";
+} else if (xp >= 10001) {
+    nivel = "Liga Radiante";
+}
+console.log(link + nivel)
+
+//Achei desnecessario colocar a ultima mensagem quando ela ja está definida e corrigida dentro dos algoritmos.
+//Fiz alguma coisa errada?
 
 
